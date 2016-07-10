595977f61dd72806ca7142e2a842579f9a158af7
Jackson Duhon <jackson_duhon@yahoo.com>
2016-07-10 00:40:08 -0700

    added some more game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,6 +4,15 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+if(cardOne === cardTwo){
+	alert("You found a match!")
+}
+else if(cardThree === cardFour){
+	alert('You found a match!')
+}
+else{
+	alert("Sorry, try again.")
+}
 
 
 console.log("JS file is connected to HTML! Woo!")
