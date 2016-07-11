6afa499c58514f855f0d220638ed6a8090bdd381
Jackson Duhon <jackson_duhon@yahoo.com>
2016-07-10 22:04:57 -0700

    created more game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,7 +4,7 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if(cardOne === cardTwo){
+/*if(cardOne === cardTwo){
 	alert("You found a match!")
 }
 else if(cardThree === cardFour){
@@ -12,7 +12,22 @@ else if(cardThree === cardFour){
 }
 else{
 	alert("Sorry, try again.")
+}*/
+
+var gameBoard = document.getElementById('game-board');
+for(var i = 0; i<4; i++){
+	document.createElement('div')[i].className = 'card';
+	document.board.appendChild('card');
 }
+
+var createBoard = function(){
+	for(var i = 0; i<4; i++){
+	document.createElement('div')[i].className = 'card';
+	document.board.appendChild('card');
+	}
+}
+
+createBoard();
 
 
 console.log("JS file is connected to HTML! Woo!")
